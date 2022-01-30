524adc8f5e2d50c37bafe3170714bd84e4dc4e40
ktkrau <ktkrau@gmail.com>
2022-01-30 03:48:18 +0000

    name generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big", "awesome"];
+let noun = ["jogger", "racoon", "bear"];
+let dominio = [".com", ".cl", ".net"];
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < dominio.length; l++)
+        console.log(pronoun[i] + adj[j] + noun[k] + dominio[l]);
+    }
+  }
+}
